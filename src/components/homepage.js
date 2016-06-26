02e5bfe8667ef48e3f6de476aaf2d03f5513d269
Jared <jaredcmurphy@gmail.com>
2016-06-26 00:23:32 -0400

    first commit

---
+++ src/components/homepage.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+let phraseList = [
+  "Web Developer",
+  "Musician",
+  "Philologist"
+];
+
+export default class Homepage extends Component {
+  render() {
+      return (
+        <div className="homepage">
+          <h1 className="title">Jared Murphy</h1>
+          <h4 className="phrase">{ phrases() }</h4>
+          <div className="square">
+            <h4 className="web">Web</h4>
+            <h4>Developer</h4>
+          </div>
+        </div>
+      );
+  }
+
+}
